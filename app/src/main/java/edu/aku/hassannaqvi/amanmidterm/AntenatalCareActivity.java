449df596cd8ce3b0fc619b8aaec3d5ed9dcaa932
shznaqvi <shznaqvi@gmail.com>
2017-05-23 15:10:21 +0500

    Innitial Comment

---
+++ app/src/main/java/edu/aku/hassannaqvi/amanmidterm/AntenatalCareActivity.java
@@ -0,0 +1,13 @@
+package edu.aku.hassannaqvi.amanmidterm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AntenatalCareActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_antenatal_care);
+    }
+}
---
+++ app/src/main/java/edu/aku/hassannaqvi/amanmidterm/PostpartumCareActivity.java
@@ -0,0 +1,13 @@
+package edu.aku.hassannaqvi.amanmidterm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PostpartumCareActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postpartum_care);
+    }
+}
