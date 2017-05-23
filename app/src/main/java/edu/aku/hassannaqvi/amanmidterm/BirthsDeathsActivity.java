6bc403f2abc326db86f12b527619f09344003aae
shznaqvi <shznaqvi@gmail.com>
2017-05-23 15:14:47 +0500

    Innitial Comment

---
+++ app/src/main/java/edu/aku/hassannaqvi/amanmidterm/BirthsDeathsActivity.java
@@ -0,0 +1,13 @@
+package edu.aku.hassannaqvi.amanmidterm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BirthsDeathsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_births_deaths);
+    }
+}
