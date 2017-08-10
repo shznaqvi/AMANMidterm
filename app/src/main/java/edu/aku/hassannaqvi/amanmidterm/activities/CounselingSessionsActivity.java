cbdb4dec5479fa2f3fc04313eceb6eded5c94b58
gul.sanober <gulsanobermemon@gmail.com>
2017-08-10 17:04:52 +0500

    education codes added

--- app/src/main/java/edu/aku/hassannaqvi/amanmidterm/activities/CounselingSessionsActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/amanmidterm/activities/CounselingSessionsActivity.java
@@ -49,16 +49,16 @@ public class CounselingSessionsActivity extends Activity {
     EditText cs02c;
     @BindView(R.id.cs0288)
     CheckBox cs0288;
-    @BindView(R.id.cs03)
-    RadioGroup cs03;
+    /*@BindView(R.id.cs03)
+    RadioGroup cs03;*/
     @BindView(R.id.cs03a)
-    RadioButton cs03a;
+    CheckBox cs03a;
     @BindView(R.id.cs03b)
-    RadioButton cs03b;
+    CheckBox cs03b;
     @BindView(R.id.cs03c)
-    RadioButton cs03c;
+    CheckBox cs03c;
     @BindView(R.id.cs0388)
-    RadioButton cs0388;
+    CheckBox cs0388;
     @BindView(R.id.cs0388x)
     EditText cs0388x;
     @BindView(R.id.cs04a)
@@ -108,7 +108,10 @@ public class CounselingSessionsActivity extends Activity {
                     cs02b.setText(null);
                     cs02c.setText(null);
                     cs0288.setChecked(false);
-                    cs03.clearCheck();
+                    cs03a.setChecked(false);
+                    cs03b.setChecked(false);
+                    cs03c.setChecked(false);
+                    cs0388.setChecked(false);
                     cs0388x.setText(null);
                     cs04a.setChecked(false);
                     cs04b.setChecked(false);
@@ -255,7 +258,10 @@ public class CounselingSessionsActivity extends Activity {
         sCs.put("cs02b", cs02b.getText().toString());
         sCs.put("cs02c", cs02c.getText().toString());
         sCs.put("cs0288", cs0288.isChecked() ? "88" : "0");
-        sCs.put("cs03", cs03a.isChecked() ? "1" : cs03b.isChecked() ? "2" : cs03c.isChecked() ? "3" : cs0388.isChecked() ? "88" : "0");
+        sCs.put("cs03a", cs03a.isChecked() ? "1" : "0");
+        sCs.put("cs03b", cs03b.isChecked() ? "2" : "0");
+        sCs.put("cs03c", cs03c.isChecked() ? "3" : "0");
+        sCs.put("cs0388", cs0388.isChecked() ? "88" : "0");
         sCs.put("cs0388x", cs0388x.getText().toString());
         sCs.put("cs04a", cs04a.isChecked() ? "1" : "0");
         sCs.put("cs04b", cs04b.isChecked() ? "2" : "0");
@@ -294,17 +300,37 @@ public class CounselingSessionsActivity extends Activity {
 
         if (cs01a.isChecked()) {
             //=================== cs02 ==============
-            if ((cs02a.getText().toString().isEmpty() || cs02b.getText().toString().isEmpty() || cs02c.getText().toString().isEmpty()) && !cs0288.isChecked()) {
+            if ((cs02a.getText().toString().isEmpty() || cs02b.getText().toString().isEmpty() || cs02c.getText().toString().isEmpty())
+                    && !cs0288.isChecked()) {
                 Toast.makeText(this, "ERROR(empty)" + getString(R.string.pc06), Toast.LENGTH_SHORT).show();
                 cs0288.setError("This data is Required!");
 
-                Log.i(TAG, "pc06: This data is Required!");
+                Log.i(TAG, "cs02: This data is Required!");
                 return false;
             } else {
                 cs0288.setError(null);
             }
 
+
             if (!cs0288.isChecked()) {
+
+                if (Integer.valueOf(cs02a.getText().toString().isEmpty() ? "0" : cs02a.getText().toString()) == 0
+                        && Integer.valueOf(cs02b.getText().toString().isEmpty() ? "0" : cs02b.getText().toString()) == 0
+                        && Integer.valueOf(cs02c.getText().toString().isEmpty() ? "0" : cs02c.getText().toString()) == 0) {
+                    Toast.makeText(this, "ERROR(invalid)" + getString(R.string.cs02), Toast.LENGTH_SHORT).show();
+                    cs02a.setError("Days, months and year can not be zero..");
+                    cs02b.setError("Days, months and year can not be zero..");
+                    cs02c.setError("Days, months and year can not be zero..");
+
+                    Log.i(TAG, "cs02: can not be zero");
+                    return false;
+                } else {
+                    cs02a.setError(null);
+                    cs02b.setError(null);
+                    cs02c.setError(null);
+                }
+
+
                 if (Integer.valueOf(cs02a.getText().toString().isEmpty() ? "0" : cs02a.getText().toString()) < 0
                         || Integer.valueOf(cs02a.getText().toString().isEmpty() ? "0" : cs02a.getText().toString()) > 29) {
                     Toast.makeText(this, "ERROR(invalid)" + getString(R.string.pc06) + " - " + getString(R.string.days), Toast.LENGTH_SHORT).show();
@@ -321,31 +347,17 @@ public class CounselingSessionsActivity extends Activity {
                     Toast.makeText(this, "ERROR(invalid)" + getString(R.string.pc06) + " - " + getString(R.string.month), Toast.LENGTH_SHORT).show();
                     cs02b.setError("Range is 0 to 11 weeks");
 
-                    Log.i(TAG, "pc06b: Range is 0 to 11 weeks");
+                    Log.i(TAG, "cs02b: Range is 0 to 11 weeks");
                     return false;
                 } else {
                     cs02b.setError(null);
                 }
 
-                if (Integer.valueOf(cs02a.getText().toString().isEmpty() ? "0" : cs02a.getText().toString()) == 0
-                        && Integer.valueOf(cs02b.getText().toString().isEmpty() ? "0" : cs02b.getText().toString()) == 0
-                        && Integer.valueOf(cs02c.getText().toString().isEmpty() ? "0" : cs02c.getText().toString()) == 0) {
-                    Toast.makeText(this, "ERROR(invalid)" + getString(R.string.cs02), Toast.LENGTH_SHORT).show();
-                    cs02a.setError("Days, months and year can not be zero..");
-                    cs02b.setError("Days, months and year can not be zero..");
-                    cs02c.setError("Days, months and year can not be zero..");
 
-                    Log.i(TAG, "bib16: can not be zero");
-                    return false;
-                } else {
-                    cs02a.setError(null);
-                    cs02b.setError(null);
-                    cs02c.setError(null);
-                }
             }
 
             //=================== cs03==============
-            if (cs03.getCheckedRadioButtonId() == -1) {
+            if (!(cs03a.isChecked() || cs03b.isChecked() || cs03c.isChecked() || cs0388.isChecked())) {
                 Toast.makeText(this, "ERROR(Empty)" + getString(R.string.cs03), Toast.LENGTH_SHORT).show();
                 cs0388.setError("This data is Required!");
                 Log.i(TAG, "cs03: This Data is Required!");
